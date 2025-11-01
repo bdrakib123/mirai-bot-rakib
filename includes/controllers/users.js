71cb93d60f7badffdd7837624480ba3f397706ac
Rakib <spyer.rakib@gmail.com>
2025-11-01 18:02:42 +0600

    Update users.js

--- includes/controllers/users.js
+++ includes/controllers/users.js
@@ -1,96 +1,96 @@
 module.exports = function ({ models, api }) {
-    const Users = models.use('Users');
+	const Users = models.use('Users');
 
-    async function getInfo(id) {
-        try {
-            const result = await api.getUserInfo(id);
-            return result[id] || null;
-        } catch (error) {
-            console.error("[Users:getInfo] ERROR:", error.stack || error.message);
-            return null;
-        }
-    }
+	async function getInfo(id) {
+		return (await api.getUserInfo(id))[id];
+	}
 
-    async function getNameUser(id) {
-        try {
-            if (global.data.userName.has(id)) return global.data.userName.get(id);
-            const data = await getData(id);
-            if (data && data.name) return data.name;
-            return "Facebook users";
-        } catch {
-            return "Facebook users";
-        }
-    }
+	async function getNameUser(id) {
+		try {
+			if (global.data.userName.has(id)) return global.data.userName.get(id);
+			else if (global.data.allUserID.includes(id)) {
+				const nameUser = (await this.getData(id)).name;
+				if (nameUser) return nameUser;
+				else return "Facebook users";
+			} else return "Facebook users";
+		}
+		catch { return "Facebook users" }
+	}
 
-    async function getAll(...data) {
-        let where = {};
-        let attributes;
-        for (const i of data) {
-            if (typeof i !== 'object') throw new Error("getAll: argument must be object or array");
-            if (Array.isArray(i)) attributes = i;
-            else where = i;
-        }
-        try {
-            const result = await Users.findAll({ where, attributes });
-            return result.map(e => e.get({ plain: true }));
-        } catch (error) {
-            console.error("[Users:getAll] ERROR:", error.stack || error.message);
-            return [];
-        }
-    }
+	async function getAll(...data) {
+		var where, attributes;
+		for (const i of data) {
+			if (typeof i != 'object') throw global.getText("users", "needObjectOrArray");
+			if (Array.isArray(i)) attributes = i;
+			else where = i;
+		}
+		try {
+			return (await Users.findAll({ where, attributes })).map(e => e.get({ plain: true }));
+		}
+		catch (error) {
+			console.error(error);
+			throw new Error(error);
+		}
+	}
 
-    async function getData(userID) {
-        try {
-            const data = await Users.findOne({ where: { userID } });
-            return data ? data.get({ plain: true }) : null;
-        } catch (error) {
-            console.error("[Users:getData] ERROR:", error.stack || error.message);
-            return null;
-        }
-    }
+	async function getData(userID) {
+		try {
+			const data = await Users.findOne({ where: { userID } });
+			if (data) return data.get({ plain: true });
+			else return false;
+		}
+		catch(error) {
+			console.error(error);
+			throw new Error(error);
+		}
+	}
 
-    async function createData(userID, defaults = {}) {
-        if (typeof defaults !== 'object' || Array.isArray(defaults)) throw new Error("createData: defaults must be object");
-        try {
-            await Users.findOrCreate({ where: { userID }, defaults });
-            return true;
-        } catch (error) {
-            console.error("[Users:createData] ERROR:", error.stack || error.message);
-            return false;
-        }
-    }
+	async function setData(userID, options = {}) {
+		if (typeof options != 'object' && !Array.isArray(options)) throw global.getText("users", "needObject");
+		try {
+			(await Users.findOne({ where: { userID } })).update(options);
+			return true;
+		}
+		catch (error) {
+			try {
+				await this.createData(userID, options);
+			} catch (error) {
+				console.error(error);
+				throw new Error(error);
+			}
+		}
+	}
 
-    async function setData(userID, options = {}) {
-        if (typeof options !== 'object' || Array.isArray(options)) throw new Error("setData: options must be object");
-        try {
-            const record = await Users.findOne({ where: { userID } });
-            if (record) await record.update(options);
-            else await createData(userID, options);
-            return true;
-        } catch (error) {
-            console.error("[Users:setData] ERROR:", error.stack || error.message);
-            return false;
-        }
-    }
+	async function delData(userID) {
+		try {
+			(await Users.findOne({ where: { userID } })).destroy();
+			return true;
+		}
+		catch (error) {
+			console.error(error);
+			throw new Error(error);
+		}
+	}
 
-    async function delData(userID) {
-        try {
-            const record = await Users.findOne({ where: { userID } });
-            if (record) await record.destroy();
-            return true;
-        } catch (error) {
-            console.error("[Users:delData] ERROR:", error.stack || error.message);
-            return false;
-        }
-    }
+	async function createData(userID, defaults = {}) {
+		if (typeof defaults != 'object' && !Array.isArray(defaults)) throw global.getText("users", "needObject");
+		try {
+			await Users.findOrCreate({ where: { userID }, defaults });
+			return true;
+		}
+		catch (error) {
+			console.error(error);
+			throw new Error(error);
+		}
+	}
 
-    return {
-        getInfo,
-        getNameUser,
-        getAll,
-        getData,
-        setData,
-        delData,
-        createData
-    };
+	return {
+		getInfo,
+		getNameUser,
+		getAll,
+		getData,
+		setData,
+		delData,
+		createData
+	};
 };
